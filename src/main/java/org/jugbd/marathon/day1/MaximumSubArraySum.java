553c7d10a7ecebbd999955736e06f8ce4cbff887
Md. Toufiqul Islam <sadydic@gmail.com>
2017-07-03 22:48:21 +0600

    Solve

--- src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
+++ src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
@@ -8,6 +8,22 @@ public class MaximumSubArraySum {
 
     public static int findMaxSum(int[] arr) {
 
-        return 4;
+		int max_privious=0, max =0;
+		
+		for(int i=0;i<arr.length;i++){
+			max += arr[i];
+			if(max<0){
+				max =0;
+			}
+			
+			else if(max>max_privious){
+				max_privious = max;
+			}
+		}
+		
+		return max_privious;
+		
+       
     }
+	 
 }
